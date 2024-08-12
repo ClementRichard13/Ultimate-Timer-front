3452a693af159b82270975d30eccc00c8ac6452e
Clement.Richard13 <richard.clement13@gmail.com>
2024-08-12 16:05:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let BACKEND_URL = 'https://zsbx3ntuki.eu-west-3.awsapprunner.com/time';
+
+
+if (BACKEND_URL[BACKEND_URL.length - 1] === '/') {
+  BACKEND_URL = BACKEND_URL.slice(0, -1);
+}  
+
+setInterval(function () {
+  fetch(`${BACKEND_URL}/time`)
+    .then(response => response.json())
+    .then(data => {
+      const timer = document.getElementById('timer');
+      timer.innerHTML = data.time;
+    });
+}, 1000);
